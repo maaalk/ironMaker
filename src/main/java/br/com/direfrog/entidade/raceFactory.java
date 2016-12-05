e7991a83ea18a8e0eefb04f17c4ed05a0127d589
maaalk <maaalk@gmail.com>
2016-12-05 11:28:08 -0200

    raceFactory

---
+++ src/main/java/br/com/direfrog/entidade/raceFactory.java
@@ -0,0 +1,55 @@
+package br.com.direfrog.entidade;
+
+public class raceFactory {
+	public static Race generateRace(String raceName){
+		Race race = new Race();
+		if (raceName == "human"){
+			race.setBasePhy(5);
+			race.setBaseSpd(6);
+			race.setBaseStr(4);
+			race.setBaseAgi(3);
+			race.setBasePrw(4);
+			race.setBasePoi(4);
+			race.setBaseIntel(3);
+			race.setBaseArc(0);
+			race.setBasePer(3);
+			
+			race.setMaxPhy((new Integer[]{7,8,8}));
+			race.setMaxSpd((new Integer[]{7,7,7}));
+			race.setMaxStr((new Integer[]{6,7,8}));
+			race.setMaxAgi((new Integer[]{5,6,7}));
+			race.setMaxPrw((new Integer[]{5,6,7}));
+			race.setMaxPoi((new Integer[]{5,6,7}));
+			race.setMaxIntel((new Integer[]{5,6,7}));
+			race.setMaxArc((new Integer[]{4,6,8}));
+			race.setMaxPer((new Integer[]{5,6,7}));
+			
+		}
+		
+		if (raceName == "trollkin"){
+			race.setBasePhy(6);
+			race.setBaseSpd(5);
+			race.setBaseStr(5);
+			race.setBaseAgi(3);
+			race.setBasePrw(4);
+			race.setBasePoi(2);
+			race.setBaseIntel(3);
+			race.setBaseArc(0);
+			race.setBasePer(3);
+			
+			race.setMaxPhy((new Integer[]{8,9,10}));
+			race.setMaxSpd((new Integer[]{6,6,6}));
+			race.setMaxStr((new Integer[]{7,8,9}));
+			race.setMaxAgi((new Integer[]{5,6,7}));
+			race.setMaxPrw((new Integer[]{5,6,7}));
+			race.setMaxPoi((new Integer[]{5,6,7}));
+			race.setMaxIntel((new Integer[]{4,5,6}));
+			race.setMaxArc((new Integer[]{4,6,7}));
+			race.setMaxPer((new Integer[]{4,5,6}));
+			
+		}
+		
+		return race;
+	}
+
+}
